cbbdaad4e0a8a32e9c42e9beff320560ad4588c5
frank_lai <jianhanlai@gmail.com>
2023-06-11 23:01:03 +0800

    first version

---
+++ microservicecloud-api/src/main/java/com/frank/springcloud/entities/Dept.java
@@ -0,0 +1,51 @@
+package com.frank.springcloud.entities;
+
+import java.io.Serializable;
+
+
+//@SuppressWarnings("serial")
+//@Data
+//@Accessors(chain = true)
+//@NoArgsConstructor
+public class Dept implements Serializable // 必須序列化
+{
+	private Long deptno; // 主鍵
+	private String dname; // 部門名稱
+	private String db_source;// 來自那個數據庫，因為微服務架構可以一個服務對應一個數據庫，同一個信息被存儲到不同數據庫
+	public Long getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getDb_source() {
+		return db_source;
+	}
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
+	}
+
+	
+	
+//	public Dept(String dname) {
+//		super();
+//		this.dname = dname;
+//	}
+
+	
+//	public static void main(String[] args) {
+//		Dept dept = new Dept();
+//	}
+	
+	
+}
---
+++ microservicecloud-consumer-dept-80/src/main/java/com/frank/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,51 @@
+package com.frank.springcloud.controller;
+ 
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.frank.springcloud.entities.Dept;
+ 
+ 
+@RestController
+public class DeptController_Consumer
+{
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    
+    /**
+     * 
+     * 使用restTemplate訪問restful接口非常的簡單粗暴無腦。
+		(url, requestMap, ResponseBean.class)這三個參數分別代表 
+		REST請求地址、請求參數、HTTP響應轉換被轉換成的對像類型。
+     * */
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @RequestMapping(value="/consumer/dept/add")
+    public boolean add(Dept dept)
+    {
+         return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add", dept, Boolean.class);
+    }
+    
+    @RequestMapping(value="/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id)
+    {
+         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id, Dept.class);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value="/consumer/dept/list")
+    public List<Dept> list()
+    {
+         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list", List.class);
+    }   
+}
+ 
+
+ 
+
